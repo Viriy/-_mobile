689c864d746936fef92dc016c27aee68e32c70f3
Viriy <541239223@qq.com>
2018-03-12 20:52:35 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ mui.init();
 mui('body').on('tap', '[href^="html"],[href*="html"],[href^="tel:"],[href*="tel:"],[href^="mailto:"],[href*="mailto:"],[href^="http"],[href*="http"],[href^="www."],[href*="www."],[href^="com"],[href*="com"],[href^="php"],[href*="php"],[href^="aspx"],[href*="aspx"]', function() {
     window.top.location.href = this.href;
 });
+
+$("input").focus(function(){
+	$(".i_arrow").hide()
+}).blur(function(){
+	$(".i_arrow").show()
+})

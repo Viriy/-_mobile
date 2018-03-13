55fcf92bdddafc98e5320da7af624c6aac15733a
Viriy <541239223@qq.com>
2018-03-13 09:40:41 +0800

    change

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ mui('body').on('tap', '[href^="html"],[href*="html"],[href^="tel:"],[href*="tel:
     window.top.location.href = this.href;
 });
 
-$("input").focus(function(){
+$("input[type=color], input[type=date], input[type=datetime-local], input[type=datetime], input[type=email], input[type=month], input[type=number], input[type=password], input[type=search], input[type=tel], input[type=text], input[type=time], input[type=url], input[type=week], select, textarea").focus(function(){
 	$(".i_arrow").hide()
 }).blur(function(){
 	$(".i_arrow").show()
